405b32a2ef097a4e3de63844f2917fe013daf84f
hulk <developer1team@gmail.com>
2016-10-12 17:03:27 +0200

    Add java 8

--- Quotes/app/src/main/java/com/djl/projects/quotes/MainActivity.java
+++ Quotes/app/src/main/java/com/djl/projects/quotes/MainActivity.java
@@ -55,13 +55,8 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
     }
 
